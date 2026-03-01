b31797a48010f6a9318261cd83da2017f6ef28c8
vlvlvl182 <benasvilit@gmail.com>
2026-03-01 17:25:17 +0300

    Add files via upload

---
+++ Лаб 2/ConsoleApplication5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+#include <vector>
+std::vector<int> steps = { 2, 4, 6, 10, 20, 50, 100 };
+double simpson_rule(double (*f)(double), double a, double b, int n) {
+    if (n % 2 == 1) n++;
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+
+    for (int i = 1; i < n; i += 2)
+        sum += 4 * f(a + i * h);
+    for (int i = 2; i < n; i += 2)
+        sum += 2 * f(a + i * h);
+
+    return (h / 3) * sum;
+}
+double function1(double x) {
+    return std::cos(x) - std::sin(x);
+}
+double function2(double x) {
+    return (std::sin(0.5 * x) - 0.5) - std::sin(x);
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    for (int n : steps) {
+
+    double area1 = simpson_rule(function1, 4.0, 5.0, n);
+    double area2 = simpson_rule(function2, 5.0, 6.0, n);
+
+    double total_area = area1 + area2;
+
+    std::cout << "Площадь при значении  " << n << std::endl;
+    std::cout << "Общая площадь закрашенной фигуры:  " << total_area << std::endl;
+}
+    return 0;
+}
---
+++ Лаб 2/UnitTest3.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication5/ConsoleApplication5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+double square(double x) {
+	return x * x;
+}
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			double result = simpson_rule(square, 0.0, 3.0, 1000);
+			Assert::AreEqual(9.0, result, 0.00001, L"Значения не совпали для x^2");
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			double r1 = simpson_rule(function2, 5.0, 6.0, 100);
+			double r2 = simpson_rule(function2, 5.0, 6.0, 100);
+			Assert::AreEqual(r1, r2, 0.00001, L"Результаты одного и того же расчета различаются");
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			double result = simpson_rule(function1, 5.0, 5.0, 100);
+			Assert::AreEqual(0.0, result, 0.00001, L"Площадь на нулевом интервале должна быть 0");
+		}
+	};
+}
